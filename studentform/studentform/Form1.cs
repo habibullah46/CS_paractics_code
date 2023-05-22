30816b30bd4144571d14b74988ae0c319c585e40
Habibullah <103850736+habibullah46@users.noreply.github.com>
2023-05-22 22:58:45 +0500

    studentmanager
    sql connection with form

---
+++ studentform/studentform/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace studentform
+{
+    public partial class Form1 : Form
+    {
+        string ConString = System.Configuration.ConfigurationManager.ConnectionStrings["myconstring"].ToString();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string commandquery = "INSERT INTO c#2023 VALUES ('"+txt_regid.Text+"','"+txt_name.Text+"','"+txt_fathername.Text+"';'"+txt_mobile.Text+"','"+txt_RollNo.Text+"','"+txt_email.Text+"','"+txt_address.Text+"')";
+           SqlConnection con = new SqlConnection(ConString);
+           con.Open();
+            SqlCommand cmd = new SqlCommand(commandquery, con);
+            cmd.ExecuteNonQuery();
+            
+        }
+
+       
+    }
+}
+ 
